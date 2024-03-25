ba209473a669c24e0dfb21142c2f5c8f0442c9c6
Matthew Elliott <mkelliott3@catamount.wcu.edu>
2024-03-25 16:05:12 -0400

    added start file comment

---
+++ rock_paper_scissors.py
@@ -0,0 +1,7 @@
+###################################################################################################
+# File Name: rock_paper_scissors.py
+# Description: A program that allows the user to play against a computer in rock paper scissors,
+# for 5 rounds.
+# Author: Matthew Elliott
+# Date: 3/26/2024
+###################################################################################################
